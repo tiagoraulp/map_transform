4f013c5447b84db715edb1792e2317fa4bc6fd6c
tiago <tiago@tiagoraul-PC>
2017-05-18 18:12:21 +0100

    changed to class

--- scripts/test.py
+++ scripts/test.py
@@ -1,135 +1,132 @@
 #!/usr/bin/env python
 
 import rospy
+import tf
 
 from interactive_markers.interactive_marker_server import *
 from visualization_msgs.msg import *
 
-import tf
-import inspect
-
-
-rospy.init_node("test_marker")
-server=InteractiveMarkerServer("test_interactive_marker")
-
-
-def processBaseUpdate(x, y):
-    br = tf.TransformBroadcaster()
-    br.sendTransform((x, y, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, 0),
-                     rospy.Time.now(),
-                     "base_link",
-                     "map") 
-    print x
-    print y
-   
-def processRadiusUpdate(x):
-    int_mark = InteractiveMarker()
-    global server
-    int_mark=server.get("base")
-    int_mark.controls[0].markers[0].scale.x=x
-    int_mark.controls[0].markers[0].scale.y=x
-    server.erase("base")
-    server.insert(int_mark, processFeedback)
-    server.applyChanges()
-    print x
-
-def processFeedback(feedback):
-    p = feedback.pose.position
-    if feedback.event_type== InteractiveMarkerFeedback.POSE_UPDATE:
-        if feedback.marker_name=="base":
-            if feedback.control_name=="base_control":
-                processBaseUpdate(p.x,p.y)
-        if feedback.marker_name=="radius":
-            if feedback.control_name=="radius_control":
-                print p.x
-                print p.y
-                processRadiusUpdate(p.y*2)
-
-if __name__=="__main__":
+class Region:
+    def __init__(self):
+        self.__x=0
+        self.__y=0
+        self.__radius=1
     
+    def setPose(self, x,y):
+        self.__x=x
+        self.__y=y
 
-    # create an interactive marker for our server
-    int_marker = InteractiveMarker()
-    int_marker.header.frame_id = "map"
-    int_marker.name = "base"
-    int_marker.description = "Base circle"
-    int_marker.scale = 1.0
+    def setRad(self, rad):
+        self.__radius=rad
 
-    # create a grey box marker
-    box_marker = Marker()
-    box_marker.type = Marker.CYLINDER
-    box_marker.scale.x = 1
-    box_marker.scale.y = 1
-    box_marker.scale.z = 0.01
-    box_marker.color.r = 0.0
-    box_marker.color.g = 0.5
-    box_marker.color.b = 0.0
-    box_marker.color.a = 0.5
+    def set(self, x,y,rad):
+        self.setPose(x,y)
+        self.setRadius(rad)
 
-    # create a non-interactive control which contains the box
-    box_control = InteractiveMarkerControl()
-    box_control.always_visible = True
-    box_control.markers.append( box_marker )
+    def getX(self):
+        return self.__x
 
-    box_control.name = "base_control"
-    box_control.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
+    def getY(self):
+        return self.__y
 
-    box_control.orientation.w=1;
-    box_control.orientation.x=0;
-    box_control.orientation.y=1;
-    box_control.orientation.z=0;
-
-    int_marker.controls.append(box_control);
-
-    # add the interactive marker to our collection &
-    # tell the server to call processFeedback() when feedback arrives for it
-    server.insert(int_marker, processFeedback)
-
-    int_marker = InteractiveMarker()
-    int_marker.header.frame_id = "base_link"
-    int_marker.name = "radius"
-    int_marker.description = "Changing Radius"
-    int_marker.scale = 1.0
-    int_marker.pose.position.y=0.5
-
-    # create a grey box marker
-    box_marker = Marker()
-    box_marker.type = Marker.SPHERE
-    box_marker.scale.x = 0.05
-    box_marker.scale.y = 0.05
-    box_marker.scale.z = 0.05
-    box_marker.color.r = 1.0
-    box_marker.color.g = 0.0
-    box_marker.color.b = 0.0
-    box_marker.color.a = 1.0
-
- 
-    # create a non-interactive control which contains the box
-    box_control = InteractiveMarkerControl()
-    box_control.always_visible = True
-    box_control.markers.append( box_marker )
-
-    box_control.name = "radius_control"
-    box_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
-
-    box_control.orientation.w=1;
-    box_control.orientation.x=0;
-    box_control.orientation.y=0;
-    box_control.orientation.z=1;
-
-    int_marker.controls.append(box_control);
-    
-    print inspect.getmembers(InteractiveMarkerServer, predicate=inspect.ismethod)    
-
-
-    server.insert(int_marker, processFeedback)
-    server.applyChanges()
+    def getRad(self):
+        return self.__radius
         
-    server.insert(int_marker, processFeedback)
-    server.applyChanges()
-    # 'commit' changes and send to all clients
-    processBaseUpdate(0,0)
+class RegionsEditor:
+    def __init__(self):
+        self.server=InteractiveMarkerServer("circular_regions")
+        rospy.Timer(rospy.Duration(0.01), self.timerCallback)
+        self.br = tf.TransformBroadcaster()
+        self.region=Region()
+        self.addMarker()
 
-    rospy.spin()
+    def timerCallback(self, event):
+        self.br.sendTransform((self.region.getX(), self.region.getY(), 0),
+                tf.transformations.quaternion_from_euler(0, 0, 0),
+                rospy.Time.now(),
+                "region",
+                "map")
+        # print 'Timer called at ' + str(event.current_real.secs) \
+        #     +' seconds and '+str(event.current_real.nsecs/1000000)+' mili senconds'
 
+    def spin(self):
+        rospy.spin()
+
+    def addMarker(self):
+        region_marker = InteractiveMarker()
+        region_marker.header.frame_id = "map"
+        region_marker.name = "region"
+        region_marker.description = "Region"
+        region_marker.pose.position.x=self.region.getX()
+        region_marker.pose.position.y=self.region.getY()
+
+        circle_marker = Marker()
+        circle_marker.type = Marker.CYLINDER
+        circle_marker.scale.x = 2*self.region.getRad()
+        circle_marker.scale.y = 2*self.region.getRad()
+        circle_marker.scale.z = 0.01
+        circle_marker.color.g = 0.5
+        circle_marker.color.a = 0.5
+    
+        trans_control = InteractiveMarkerControl()
+        trans_control.always_visible = True
+        trans_control.name = "translation"
+        trans_control.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
+        trans_control.orientation.w=1;
+        trans_control.orientation.x=0;
+        trans_control.orientation.y=1;
+        trans_control.orientation.z=0;
+        trans_control.markers.append( circle_marker )
+
+        region_marker.controls.append(trans_control);
+
+        self.server.insert(region_marker, self.processFeedback)
+
+        radius_marker = InteractiveMarker()
+        radius_marker.header.frame_id = "region"
+        radius_marker.name = "radius"
+        radius_marker.description = "Changing Radius"
+        radius_marker.pose.position.x=self.region.getRad()
+
+        point_marker = Marker()
+        point_marker.type = Marker.SPHERE
+        point_marker.scale.x = 0.05
+        point_marker.scale.y = 0.05
+        point_marker.scale.z = 0.05
+        point_marker.color.r = 1.0
+        point_marker.color.a = 1.0
+
+        scale_control = InteractiveMarkerControl()
+        scale_control.always_visible = True
+        scale_control.name = "scaling"
+        scale_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+        scale_control.markers.append( point_marker )
+
+        radius_marker.controls.append(scale_control);
+    
+        self.server.insert(radius_marker, self.processFeedback)
+        self.server.applyChanges()
+ 
+    def processFeedback(self,feedback):
+        p = feedback.pose.position
+        if feedback.event_type== InteractiveMarkerFeedback.POSE_UPDATE:
+            if feedback.marker_name=="region":
+                if feedback.control_name=="translation":
+                    self.region.setPose(p.x,p.y)
+        if feedback.event_type== InteractiveMarkerFeedback.POSE_UPDATE:
+            if feedback.marker_name=="radius":
+                if feedback.control_name=="scaling":
+                    self.region.setRad(p.x)
+                    self.updateMarker("region")
+        self.server.applyChanges()
+
+    def updateMarker(self, name):
+        region_marker=self.server.get(name)
+        region_marker.controls[0].markers[0].scale.x=self.region.getRad()*2
+        region_marker.controls[0].markers[0].scale.y=self.region.getRad()*2
+        self.server.insert(region_marker, self.processFeedback)
+        
+if __name__=="__main__":    
+    rospy.init_node("test_marker")
+    regionsEdit=RegionsEditor()
+    regionsEdit.spin()
